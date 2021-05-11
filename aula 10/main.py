2355c05d04ade35b1f2fe2c9178f5a6e57dc6c24
Azenha <azenha.leonardo@gmail.com>
2021-05-10 21:54:16 -0300

    Aula 10

---
+++ aula 10/main.py
@@ -0,0 +1,16 @@
+from No import No
+from Lista import Lista
+
+
+lista = Lista()
+lista.imprimir()
+
+lista.adicionar(5)
+lista.imprimir()
+
+lista.adicionar(2)
+lista.imprimir()
+
+lista.excluir(3)
+lista.excluir(5)
+lista.excluir(2)
