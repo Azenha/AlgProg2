71032a9e11b472ca947b622a9bf49b1f217b0657
Leonardo Azenha <Azenha@users.noreply.github.com>
2021-06-14 22:31:05 -0300

    Com gabarito
    Gabarito

--- aula 13/Pilha.py
+++ aula 13/Pilha.py
@@ -5,27 +5,66 @@ class Pilha:
     def __init__(self):
         self.topo = None
         self.tamanho = 0
+    
+    """ Exemplo da aula
+    def adicionar(self, valor):
+		no = No( valor )
+		if self.tamanho == 0 :
+			self.topo = no
+		else: 
+			no.proximo = self.topo
+			self.topo = no
+		self.tamanho +=1
+#		if self.tamanho > 0 :
+#			no.proximo = self.topo
+#		self.topo = no
+    """
 
     def empilhar(self, elemento):
         no = No(elemento)
         no.proximo = self.topo
         self.topo = no
-        self.tamanho = self.tamanho + 1
+        self.tamanho += 1
 
     def remover(self):
         if self.tamanho > 0:
             no = self.topo
             self.topo = self.topo.proximo
-            self.tamanho = self.tamanho - 1
+            self.tamanho -= 1
             return no.dado
         # raise IndexError("A pilha está vazia")
         print("A pilha está vazia")
+    
+    """ Exemplo da aula
+    def remover(self):
+		if self.tamanho == 0:
+			print( "Pilha vazia!" )
+		elif self.tamanho == 1:
+			self.topo = None
+			self.tamanho = 0
+		else:
+			self.topo = self.topo.proximo
+			self.tamanho -= 1
+		self.imprimir()
+    """
 
     def espiar(self):
         if self.tamanho > 0:
             return self.topo.dado
         # raise IndexError("A pilha está vazia")
         print("A pilha está vazia")
+
+    """ Exemplo da aula
+   	def imprimir(self):
+		# if self.topo == None:
+		if self.tamanho == 0:
+			print( "Pilha vazia!" )
+		else:
+			aux = self.topo
+			while( aux ) :
+				print( aux.dado )
+				aux = aux.proximo
+    """
 
     def __len__(self):
         return self.tamanho
